7a1b3c932d91f0b7a5243a68893b52964ef6fcc1
ps807 <66031843+ps807@users.noreply.github.com>
2020-10-08 20:44:23 +0200

    END

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [submittedName, setSubmittedName] = useState("");
+
+  function handleChanged(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    setSubmittedName(name);
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {submittedName}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChanged}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
